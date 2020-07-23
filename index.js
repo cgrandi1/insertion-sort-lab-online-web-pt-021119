86149bf88a69901a60c0a670695edff6476a837b
github username <carltondgrandison@gmail.com>
2020-07-22 23:14:11 -0600

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 function findMinAndRemove(array){
+    let min = array[0];
+    let minIndex = 0;
+    for (let i = 0; i < array.length; i++){
+        let currentElement = array[i]
+        if(currentElement < min){
+            min = array[i];
+            minIndex = i
+        }
+    }
+    array.splice(minIndex, 1)
+    return min
 
 }
 
 function selectionSort(array){
+    let newMin;
+    let sort = [];
+    while (array.length != 0){
+        newMin = findMinAndRemove(array)
+        sort.push(newMin)
+    }
+    return sort
 
 }
